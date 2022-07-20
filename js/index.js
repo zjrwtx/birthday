01d6dae070ee37d28c72167501e35ef7251d551e
正经人王同学 <86822589+zjrwtx@users.noreply.github.com>
2022-07-20 18:53:55 +0800

    1

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3|王|导|演|祝|你|生|日|快|乐|#rectangle|');
+      S.UI.simulate('|#countdown 3|祝|你|生|日|快|乐|#rectangle|');
     }
 
     S.Drawing.loop(function () {
